a9500b0e0bdd8870b9dda36aa2aae85582dda158
Ozan Turgut <ozan@signalfuse.com>
2014-11-24 12:08:59 -0800

    :sunrise:

---
+++ bin/server.js
@@ -0,0 +1,37 @@
+var knox = require('knox');
+var express = require('express');
+
+var argv = require('minimist')(process.argv.slice(2));
+
+var bucket = argv.bucket || process.env.S3_SERVER_BUCKET;
+var key = argv.key || process.env.AWS_ACCESS_KEY_ID;
+var secret = argv.secret || process.env.AWS_SECRET_ACCESS_KEY;
+var port = argv.p || argv.port || process.env.S3_SERVER_PORT || 3010;
+
+console.log('Serving ' + bucket + ' on port ' + port);
+
+var client = knox.createClient({
+  key: key,
+  secret: secret,
+  bucket: bucket
+});
+
+var app = express();
+
+app.use(function(req, res, next){
+  client.get(req.path).on('response', function(awsRes){
+    res.set(awsRes.headers);
+    res.status(awsRes.statusCode);
+    
+    awsRes.setEncoding('utf8');
+    awsRes.on('data', function(chunk){
+      res.write(chunk);
+    });
+
+    res.on('end', function(){
+      res.end();
+    });
+  }).end();
+});
+
+app.listen(port);
