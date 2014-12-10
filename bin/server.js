e42ccdbb4ba6ed97e0a971d69c0f4e83a127374f
Ozan Turgut <ozan@signalfuse.com>
2014-12-09 20:08:09 -0800

    Added a list page to show prefixes

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
-var knox = require('knox');
 var express = require('express');
-
+var AWS = require('aws-sdk');
 var argv = require('minimist')(process.argv.slice(2));
+var path = require('path');
+var fs = require('fs');
+var Mustache = require('mustache');
+
+// Load the list html
+var listHtmlFile = path.resolve(__dirname, '../lib/list.html');
+var listHtml = fs.readFileSync(listHtmlFile).toString();
+Mustache.parse(listHtml);
 
 var bucket = argv.bucket || process.env.S3_SERVER_BUCKET;
 var key = argv.key || process.env.AWS_ACCESS_KEY_ID;
@@ -12,37 +19,81 @@ var port = argv.p || argv.port || process.env.S3_SERVER_PORT || 3010;
 
 console.log('Serving ' + bucket + ' on port ' + port);
 
-var client = knox.createClient({
-  key: key,
-  secret: secret,
-  bucket: bucket
+AWS.config.update({
+  accessKeyId: key,
+  secretAccessKey: secret
 });
+var s3 = new AWS.S3();
 
 var app = express();
 
-app.use(function(req, res, next){
-  var path = req.path;
-  
-  if(path.slice(-1) === '/') path += 'index.html';
-  
-  client.get(path).on('response', function(awsRes){
-    res.set(awsRes.headers);
+function loadPrefixes(prefix, callback){
+  s3.listObjects({
+    Bucket: bucket,
+    Delimiter: '/',
+    EncodingType: 'url',
+    Prefix: prefix
+  }, callback); 
+}
 
-    res.status(awsRes.statusCode);
-    
-    //awsRes.setEncoding('utf8');
-    awsRes.on('data', function(chunk){
-      res.write(chunk);
-    });
-
-    res.on('end', function(){
+function serve(path, res){
+  s3.getObject({ Bucket: bucket, Key: path }, function(err, data){
+    if(err){
+      res.status(err.statusCode);
       res.end();
+      return;
+    } else {
+      res.status(200);
+    }
+
+    res.set({
+      'Content-Length': data.ContentLength,
+      'Last-Modified': data.LastModified,
+      'Expiration': data.Expiration,
+      'Etag': data.ETag,
+      'Content-Encoding': data.ContentEncoding,
+      'Content-Type': data.ContentType,
     });
 
-    res.on('error', function(err){
-      console.error(req.path, err);
+    res.write(data.Body);
+    res.end();
+  });
+}
+
+function serveList(prefixes, res){
+  res.write(Mustache.render(listHtml, {
+    prefixes: prefixes,
+    s3Bucket: bucket
+  }));
+  res.end();
+}
+
+app.use(function(req, res, next){
+  var path = req.path.substr(1);
+  
+  if(path === '' || path.slice(-1) === '/'){
+    loadPrefixes(path, function(err, data){
+      if(data.Contents.length){
+        var indexPath;
+        data.Contents.some(function(obj){
+          if (obj.Key.indexOf('index.html') !== -1){
+            indexPath = obj.Key;
+            return true;
+          }
+        });
+
+        if(indexPath) {
+          serve(indexPath, res);
+        } else {
+          serveList(data.CommonPrefixes, res);
+        }
+      } else {
+        serveList(data.CommonPrefixes, res);
+      }
     });
-  }).end();
+  } else {
+    serve(path, res);
+  }
 });
 
 app.listen(port);
